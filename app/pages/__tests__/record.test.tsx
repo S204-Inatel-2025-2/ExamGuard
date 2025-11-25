e030a8549e63c2d76e715f4dc4ce364d2930a17f
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-25 15:47:08 -0300

    fix: analise estatica

--- app/pages/__tests__/record.test.tsx
+++ app/pages/__tests__/record.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Route, Routes, useParams } from "react-router";
 import Record from "../record";
