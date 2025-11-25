9098e0be04a062e07f0d9ca2337822362b387551
Henrique Pizzoni <133734395+henrique-pizzoni@users.noreply.github.com>
2025-11-25 12:09:49 -0300

    Uncomment Content-Type header in axios client

--- app/services/axios-backend-client.ts
+++ app/services/axios-backend-client.ts
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     headers: {
-        //'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
         Accept: 'application/json',
     },
 });
